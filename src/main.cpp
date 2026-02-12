39d6ef3269ad75dea809b94a3c0a0142a4e76e61
Vialcore <egovein001@gmail.com>
2026-02-12 13:08:31 -0800

    window display changes and event handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,41 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <algorithm>
 #include <iostream>
 
 int main()
 {
-    std::cout<<"works";
-    return 0;
+    sf::RenderWindow window(sf::VideoMode({800,600}), "HyperManifold");
+    //window.setVerticalSyncEnabled(true);  //maybe don't use this with setFramerateLimit()
+    window.setFramerateLimit(60); //internally uses sf::sleep...might need to change later
+
+    //Player entity; might need to make separate class
+    sf::RectangleShape player({40.f,60.f});
+
+    //Ground entity 
+    sf::Vector2f posY(0.f,450.f);
+    sf::RectangleShape ground({960.f,40.f});
+    ground.setPosition(posY);
+
+    while(window.isOpen())
+    {
+        //pollEvent()is method using FIFO event queue so always call it in A LOOP
+        while(const std::optional event = window.pollEvent())
+        {
+            if(event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+        window.clear(sf::Color::Black);
+
+
+
+        //typically, call it after all OpenGL rendering is done
+        window.display(); //this is double-buffering; buffer copies and draws it to window
+    }//end of "window open" while loop
+
+    std::cout<<"Thank you for playing my game!";
 }
 
 
